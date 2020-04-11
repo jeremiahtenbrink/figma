50682d66d7d84603a1c84e59e0629be388dde7e1
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2020-04-11 03:07:15 -0600

    Added some more Icons

--- svg-fun/src/views/IconsOnPath.js
+++ svg-fun/src/views/IconsOnPath.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import { ReactComponent as SVG } from "../svgs/IconSidePaths.svg";
 import { gsap } from "gsap";
@@ -12,8 +12,42 @@ gsap.registerPlugin( MotionPathPlugin );
  *
  */
 const IconsOnPath = ( props ) => {
-  const path = document.querySelector( "#motionPath" );
-  const icons = document.querySelctor
+  
+  useEffect( () => {
+    debugger;
+    const icons = document.querySelector( "#icons" );
+    if( icons.childElementCount > 0 ){
+      
+      let child = icons.firstElementChild;
+      const id = "#" + child.getAttribute( "id" );
+      gsap.to( id, {
+        motionPath: {
+          path: "#motionPath",
+          align: "#motionPath",
+          alignOrigin: [ .5, .5 ],
+          autoRotate: false,
+        }, duration: "240", repeat: -1, ease: "none",
+        
+      } );
+      let number = 0;
+      while( child.nextElementSibling ){
+        number += 1;
+        child = child.nextElementSibling;
+        const id = "#" + child.getAttribute( "id" );
+        gsap.to( id, {
+          motionPath: {
+            path: "#motionPath",
+            align: "#motionPath",
+            alignOrigin: [ .5, .5 ],
+            autoRotate: false,
+            start: ( .01 * number ),
+            ease: "none",
+          }, duration: "240", repeat: -1, ease: "none",
+        } );
+      }
+    }
+  }, [] );
+  
   return ( <Container>
     <SVG/>
   </Container> );
